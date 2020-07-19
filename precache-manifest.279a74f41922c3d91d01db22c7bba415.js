c8da1ae2ab3f147466786c57432af2bbb0db825e
miz2121 <miz2121@naver.com>
2020-07-19 16:26:25 +0900

    Updates

---
+++ precache-manifest.279a74f41922c3d91d01db22c7bba415.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb004b332667bf5213844e9346ef292b",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "73180824958b83f2530e",
+    "url": "/movie_app_2020/static/css/main.21bc3d3e.chunk.css"
+  },
+  {
+    "revision": "1c27731e27d4248cd146",
+    "url": "/movie_app_2020/static/js/2.d6abe422.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.d6abe422.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73180824958b83f2530e",
+    "url": "/movie_app_2020/static/js/main.00062fd6.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
